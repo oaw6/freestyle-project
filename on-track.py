8afe2be15b3d557148b6b936f1be987444c09a53
Owen <oaw6@georgetown.edu>
2019-05-02 15:44:40 -0400

    Cleanup and successful event.get code

--- on-track.py
+++ on-track.py
@@ -9,28 +9,9 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from httplib2 import Http
 
-#SCOPES = 'https://www.googleapis.com/auth/calendar'
-#store = file.Storage('storage.json')
-#creds = store.get()
-#if not creds or creds.invalid:
-#    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
-#    creds = tools.run_flow(flow, store)
-#
-#CALENDAR = apiclient.discovery.build('calendar', 'v3', http=creds.authorize(Http()))
-#GMT_OFF = '-04:00' # EST/GMT-7
-#EVENT = {
-#    'summary': 'Dinner with friends',
-#    'start': {'dateTime': '2019-05-03T19:00:00%s' % GMT_OFF},
-#    'end':   {'dateTime': '2019-05-03T22:00:00%s' % GMT_OFF},
-#    'attendees': [],
-#}
-#CALENDAR.events().insert(calendarId='primary', body=EVENT).execute()
-
 # If modifying these scopes, delete the file token.pickle.
-#SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
-#def main():
 def tenevents():
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
@@ -107,11 +88,9 @@ def addnewevent(summary_text, start_time, end_time, attendees, custom_event_id):
     service.events().insert(calendarId='primary', body=EVENT).execute()
 
 
+
 def deleteevent(custom_event_id):
     creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
@@ -133,7 +112,31 @@ def deleteevent(custom_event_id):
     #Deletes the event
     service.events().delete(calendarId='primary', eventId=custom_event_id).execute()
 
-#tenevents()
+
+
+def getevent(custom_event_id):
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                #'credentials.json', SCOPES)
+                'client_id.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    #Gets and prints the event
+    event = service.events().get(calendarId='primary', eventId=custom_event_id).execute()
+    print(event['summary'])
 
 GMT_OFF = '-04:00'
 
@@ -141,7 +144,8 @@ summary_test = 'Dinner with friends'
 start_time_test = {'dateTime': '2019-05-03T11:00:00%s' % GMT_OFF}
 end_time_test = {'dateTime': '2019-05-03T12:00:00%s' % GMT_OFF}
 attendees_test = []
-event_id_test = '20190503110000dinner1511it'
+event_id_test = 'so20190503110000dinner1511it'
 
 #addnewevent(summary_test, start_time_test, end_time_test, attendees_test, event_id_test)
-deleteevent(event_id_test)
+#deleteevent(event_id_test)
+#getevent(event_id_test)
